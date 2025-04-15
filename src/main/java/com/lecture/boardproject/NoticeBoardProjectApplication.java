49e9fe6bfd08535839bc71f81885646512dec61b
yys <yeseul7002@gmail.com>
2025-04-15 10:08:01 +0900

    #5 - 스프링부트 프로젝트 init
    * 스프링웹, 액츄에이터, 데브툴즈,롬복

---
+++ src/main/java/com/lecture/boardproject/NoticeBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lecture.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoardProjectApplication.class, args);
+    }
+
+}
